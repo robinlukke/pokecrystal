8d06090e03a50be4dc13d2a095cf1ca4eeefe66d
robinlukke <47284200+robinlukke@users.noreply.github.com>
2022-09-27 14:02:29 -0300

    revamped trainer edit

--- main.asm
+++ main.asm
@@ -157,6 +157,8 @@ INCLUDE "engine/battle/ai/scoring.asm"
 INCLUDE "engine/battle/read_trainer_attributes.asm"
 INCLUDE "engine/battle/read_trainer_party.asm"
 
+INCLUDE "data/trainers/parties.asm"
+
 
 SECTION "Battle Core", ROMX
 
