00882e3942a67eb3425b14227675c34c5a11b9c0
robinlukke <47284200+robinlukke@users.noreply.github.com>
2022-10-01 08:15:33 -0300

    Update attributes.asm

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -70,7 +70,7 @@ TrainerClassAttributes:
 
 ; Cal
 	db NO_ITEM, NO_ITEM ; items
-	db 25 ; base reward
+	db 1 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
