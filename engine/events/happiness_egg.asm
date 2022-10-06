c1868a2fe0a9e4dd6cc7247de720c93689053b3f
robinlukke <47284200+robinlukke@users.noreply.github.com>
2022-10-06 00:31:13 -0300

    Update happiness_egg.asm
    higher chances to get an egg

--- engine/events/happiness_egg.asm
+++ engine/events/happiness_egg.asm
@@ -212,17 +212,17 @@ DayCareStep::
 	callfar CheckBreedmonCompatibility
 	ld a, [wBreedingCompatibility]
 	cp 230
-	ld b, 31 percent + 1
+	ld b, 90 percent
 	jr nc, .okay
 	ld a, [wBreedingCompatibility]
 	cp 170
-	ld b, 16 percent
+	ld b, 80 percent
 	jr nc, .okay
 	ld a, [wBreedingCompatibility]
 	cp 110
-	ld b, 12 percent
+	ld b, 70 percent
 	jr nc, .okay
-	ld b, 4 percent
+	ld b, 60 percent
 
 .okay
 	call Random
